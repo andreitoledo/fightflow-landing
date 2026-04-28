2118d06f36605d27fab85f3c2e136d7ee5a7b3bc
andreitoledo <andreiltoledo@hotmail.com>
2026-04-28 19:09:10 -0300

    feat(frontend): integrar google analytics ga4

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import App from './App';
-import { appTheme } from './theme/appTheme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import App from "./App";
+import { appTheme } from "./theme/appTheme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// GA4
+const GA_MEASUREMENT_ID = "G-FX85WZZVY0"; // <-- troque pelo seu
+
+if (typeof window !== "undefined") {
+  const script1 = document.createElement("script");
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', '${GA_MEASUREMENT_ID}');
+  `;
+  document.head.appendChild(script2);
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={appTheme}>
       <CssBaseline />
